3f1c1cc41ed9f81ceebfce9f539cb55708375220
Kojapu <163666238+Kojapu@users.noreply.github.com>
2025-06-29 21:33:07 +0200

    Add incrementation to the score

--- script.js
+++ script.js
@@ -20,22 +20,28 @@ let computerScore = 0;
 function playRound(humanChoice, computerChoice) {
     if (humanChoice === "rock" && computerChoice === "paper") {
         console.log("You lose! Paper beats Rock.");
+        computerScore++;
     } else if (humanChoice === "paper" && computerChoice === "paper") {
         console.log("It's a draw! Paper vs Paper.");
     } else if (humanChoice === "scissor" && computerChoice === "paper") {
         console.log("You win! Scissor beats Paper.");
+        humanScore++;
     } else if (humanChoice === "rock" && computerChoice === "scissor") {
         console.log("You win! Rock beats Scissor.");
+        humanScore++;
     } else if (humanChoice === "paper" && computerChoice === "scissor") {
         console.log("You lose! Scissor beats Paper.");
+        computerScore++;
     } else if (humanChoice === "scissor" && computerChoice === "scissor") {
         console.log("It's a draw! Scissor vs Scissor.");
     } else if (humanChoice === "rock" && computerChoice === "rock") {
         console.log("It's a draw! Rock vs Rock.");
     } else if (humanChoice === "paper" && computerChoice === "rock") {
         console.log("You win! Paper beats Rock.");
+        humanScore++;
     } else if (humanChoice === "scissor" && computerChoice === "rock") {
         console.log("You lose! Rock beats Scissor.");
+        computerScore++;
     }
 }
 
