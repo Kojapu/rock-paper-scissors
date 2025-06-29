50587745d774d54663c2e44dfd6f32c63e13857f
Kojapu <163666238+Kojapu@users.noreply.github.com>
2025-06-29 20:38:06 +0200

    Add logic for computer choice
    This commit adds the logic for the computer's choice in the game.

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function getComputerChoice() {
+    let random = Math.random();
 
-} 
+    if (random < 1/3) {
+        choice = "rock";
+    } else if (random < 2/3) {
+        choice = "paper";
+    } else {
+        choice = "scissor";
+    }
+}
